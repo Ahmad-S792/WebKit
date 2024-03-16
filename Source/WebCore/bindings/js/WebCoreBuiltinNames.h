60ce73139e3c838f96847d43606ae24c0f4dde50
Ahmad-S792 <52317531+Ahmad-S792@users.noreply.github.com>
2024-03-16 05:12:14 +0000

    Sync `DeviceMotionEvent` as per web specification
    https://bugs.webkit.org/show_bug.cgi?id=271098
    rdar://problem/125271931
    
    Reviewed by NOBODY (OOPS!).
    
    This patch aligns WebKit with Blink / Chrome and Web Specification [1]:
    
    [1] https://w3c.github.io/deviceorientation/#device-motion-event-api
    
    By removing `initDeviceMotionEvent`, WebKit aligns with Web Specification
    and Blink also removed it in 2017 via below commit:
    
    Reference: https://chromium.googlesource.com/chromium/src.git/+/0921e8319f228e406019b434701ac9f181ee8ea1
    
    Additionally, it adds [SecureContext] and also make 'interval'
    non-nullable. Also adding '=null' values for 'Accelerate'
    and 'Rotate' dictonaries.
    
    NOTE: We cannot update test to leverage constructor, since
    we don't have support for it.
    
    e.g., `event.initDeviceMotionEvent` changed to `event = new DeviceMotionEvent`
    
    Additionally, we have same test as WPT with similar name.
    Although, it is `https` due to being `SecureContext`.
    For future, I am adding few FIXME as well in IDL file to align
    with web specification.
    
    * Source/WebCore/dom/DeviceMotionEvent.cpp:
    (convert): DeviceMotionEvent::Acceleration
    (convert): DeviceMotionEvent::RotationRate
    (DeviceMotionEvent::initDeviceMotionEvent):
    * Source/WebCore/dom/DeviceMotionEvent.h:
    * Source/WebCore/dom/DeviceMotionEvent.idl:
    * Source/WebCore/bindings/js/WebCoreBuiltinNames.h: Add macro for 'SecureContext'
    * Source/WebCore/dom/DeviceMotionData.cpp:
    (DeviceMotionData::create): Update argument to remove std::optional<>
    (DeviceMotionData::DeviceMotionData): Ditto
    * Source/WebCore/dom/DeviceMotionData.h:
    * Source/WebCore/dom/DeviceMotionEvent.cpp:
    (DeviceMotionEvent::interval):
    * Source/WebCore/dom/DeviceMotionEvent.h:
    * LayoutTests/fast/dom/DeviceMotion/optional-event-properties.html: Rebaselined
    * LayoutTests/fast/dom/DeviceMotion/optional-event-properties-expected.txt: Rebaselined

--- Source/WebCore/bindings/js/WebCoreBuiltinNames.h
+++ Source/WebCore/bindings/js/WebCoreBuiltinNames.h
@@ -182,6 +182,7 @@ namespace WebCore {
     macro(DecompressionStreamTransform) \
     macro(DelayNode) \
     macro(DeprecationReportBody) \
+    macro(DeviceMotionEvent) \
     macro(DigitalCredential) \
     macro(DocumentTimeline) \
     macro(DynamicsCompressorNode) \
